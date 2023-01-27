33f1ebf8225a1a14233b99402ef8ff79f14f80bf
ROBERT <robertlagat38@gmail.com>
2023-01-27 12:53:06 +0300

    third

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,38 +1,142 @@
 
 const { io } = require('../index');
 const Product = require('../models/product.model');
+const Coupon = require('../models/Coupon.model');
+const Category = require('../models/category.model');
+const slugify = require('slugify');
+const crypto = require('crypto');
 
 
 
+exports.product_create = function (req, res, next) {
+    // Generate a new SKU
+    function generateSKU() {
+        let sku = crypto.randomBytes(4).toString('hex');
+        return sku.toUpperCase();
+    }
+
+    function generateQuantity() {
+        return Math.floor(Math.random() * (1000 - 100 + 1) + 100);
+    }
+
+    const { title, unit, parent, children, image, originalPrice, discount, description, type,flashSale, status } = req.body;
+    const price = originalPrice - (originalPrice * (discount/100));
+    const slug = slugify(title, { lower: true }); // create a slug from the title
+    const sku = generateSKU();
+    const quantity = generateQuantity();
+
+    const newProduct = new Product({
+        title,
+        unit,
+        parent,
+        children,
+        image,
+        originalPrice,
+        price,
+        discount,
+        quantity,
+        description,
+        type,
+        flashSale,
+        status,
+        slug,
+        sku
+    });
+    
+    newProduct.save()
+        .then(product => {
+            res.status(201).json({
+                message: 'Product added successfully',
+                product
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+    };
+
+    
+exports.product_coupon=async function (req, res) {
+
+    try {
+        // Find all coupons in the database
+        const coupons = await Coupon.find();
+        // Render the coupons on the page
+        res.render('../views/coupon.ejs', { coupons });
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error loading coupons' });
+    }
+};
+    
+
+exports.coupon_create= async function (req, res) {
+    try {
+        // create a new coupon with the request data
+        const coupon = new Coupon(req.body);
+
+        // save the coupon to the database
+        await coupon.save();
+
+        // send a 201 status and the new coupon as the response
+        res.status(201).json(coupon);
+    } catch(err) {
+        // if there's an error, send a 500 status and the error message
+        console.error(err);
+        res.status(500).json({ message: 'Error adding coupon' });
+    }
+};
+  
+
+exports.product_category=async function (req, res) {
+
+    try {
+        // Find all coupons in the database
+        const categories = await Category.find();
+        // Render the coupons on the page
+        res.render('../views/category.ejs', { categories });
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error loading coupons' });
+    }
+};
+    
+
+exports.category_create= async function (req, res) {
+    try {
+        // Create a new category with the request data
+        const { parent,type,icon,status } = req.body;
+        const slug = slugify(parent, { lower: true }); // create a slug from the title
+
+        
+    const newCategory = new Category({
+       parent,
+       type,
+       icon,
+       slug,
+       status
+    });
+        // Save the category to the database
+        await newCategory.save();
+
+        // Send a 201 status and the new category as the response
+        res.status(201).json(newCategory);
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error adding category' });
+    }
+};
+  
 
 exports.test = function (req, res) {
     res.send('Greetings from the Test controller!');
 };
 
-exports.product_create = function (req, res, next) {
-    const { name, price } = req.body;
-    if (!name || !price) {
-       
-        io.emit("flashMessage", {type: 'error', message: 'Both Name and Price fields are required'});
-    }
 
-    const product = new Product({ name, price });
 
-    product.save(function (err) {
-        if (err) {
-            return next(err);
-        }
 
-        // Send the new product data to connected clients
-        io.emit("newProduct",  product );
-
-        io.emit("flashMessage", { type: 'success', message: "New product created!" });
-
-        // Send a success message to the client
-        res.setHeader('Location', req.originalUrl);
-        res.status(200).json({message:'Product Created'});
-    });
-};
 
 
 
@@ -128,3 +232,5 @@ exports.product_details = function (req, res) {
         });
       };
       
+
+    
--- routes/product.route.js
+++ routes/product.route.js
@@ -10,11 +10,11 @@ const product_controller = require('../controllers/product.controller');
 // a simple test url to check that all of our files are communicating correctly.
 router.get('/test', product_controller.test);
 router.post('/create', product_controller.product_create);
+router.get('/coupon', product_controller.product_coupon);
+router.post('/coupon_create', product_controller.coupon_create);
+router.get('/category', product_controller.product_category);
+router.post('/category_create', product_controller.category_create);
 router.get('/all',cors(), product_controller.product_details);
-router.put('/:productId/update', product_controller.product_update);
-router.delete('/:id/deletion', product_controller.product_delete);
-router.get("/loadmore/:skip/:limit", product_controller.product_more);
-router.get('/:productId', product_controller.product_find);
 
 
 module.exports = router;
